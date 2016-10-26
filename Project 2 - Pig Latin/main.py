10b67091a1b720f089fb591c24c00b0dd0613dcd
Pyrophoenix100 <AlecTuchscherer@hasd.org>
2016-10-26 14:46:02 -0500

    Finished Project 2

--- Project 2 - Pig Latin/main.py
+++ Project 2 - Pig Latin/main.py
@@ -1,3 +1,16 @@
 import os
 import sys
+import time
 sys.path.append(os.path.abspath(".."))
+import projectmodule
+projectmodule.printheader("Pig Latin", 2, "10/25/2016", "10/26/2016", "This program translates whatever you type in into pig latin.")
+base = input("> ")
+for x in base.split(" "):
+	first = x[0:1]
+	if len(x) > 1:
+		sys.stdout.write(x[1::] + "-" + first + "ay ")
+	else:
+		sys.stdout.write(first + "ay ")
+	
+print("")
+end = input("Press enter to continue...")
