92fb86488ceacde25351dccaea7d59b3beca46f6
Pyrophoenix100 <alectuc@gmail.com>
2016-10-26 10:59:57 -0500

    Made project module
    Made a module to add a header to projects

--- Project 1 - Reverse a String/main.py
+++ Project 1 - Reverse a String/main.py
@@ -1,5 +1,12 @@
 import time
 import sys
+time.sleep(1)
+print(sys.path)
+time.sleep(1)
+sys.path.append('\..')
+print(sys.path)
+time.sleep(1)
+
 sys.stdout.write("> ")
 inputs = input()
 print(inputs[::-1])
---
+++ projectmodule/projectmodule.py
@@ -0,0 +1,6 @@
+def printheader(projectname, projectnum, startdate, enddate):
+	print("======================================")
+	print("100 Coding Projects")
+	print("Project #%d - %s" % (projectnum, projectname))
+	print("Started: %s" % startdate)
+	print("Completed: %s" % enddate)
