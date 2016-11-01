e49729becf3db4642d5ebdc701cabc27e86b4d58
Pyrophoenix100 <alectuc@gmail.com>
2016-11-01 11:24:41 -0500

    Project3

---
+++ Project 3 - Counting Vowels/main.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import time
+sys.path.append(os.path.abspath(".."))
+import projectmodule
+vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+projectmodule.printheader("Counting Vowels", 3, "11/1/2016", "N\A", "This program counts the vowels in whatever you type in.")
+base = input()
+numvowels = 0
+numconsanants = 0 
+for x in base:
+	if x in vowels:
+		numvowels += 1
+	elif x not in "1234567890 -=_+\][';/.,!@#$%^&*()`~<>?:{}|":	
+		numconsanants += 1
+	elif x not in """ .,!?$()@#%^&*'""'""":
+		print("Please stop trying to break the program.")
+print("Vowels = %d" % numvowels)
+print("Consonants = %d" % numconsanants)
+time.sleep(5)
