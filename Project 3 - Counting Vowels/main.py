b34dd77b23138aac1f16bb08934adea70af068da
Pyrophoenix100 <alectuc@gmail.com>
2016-11-03 11:06:29 -0500

    Small changes
    I changed a few things I didn't like. For example, changing one of the
    challenges and adding a input marker to project 3. I also changed the
    finish date to project 3 because I found a couple bugs.

--- Project 3 - Counting Vowels/main.py
+++ Project 3 - Counting Vowels/main.py
@@ -5,16 +5,22 @@ sys.path.append(os.path.abspath(".."))
 import projectmodule
 vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 projectmodule.printheader("Counting Vowels", 3, "11/1/2016", "11/2/2016", "This program counts the vowels in whatever you type in.")
-base = input()
+base = input("> ")
 numvowels = 0
+consonantarray = []
 numconsanants = 0 
+consonants = ""
 for x in base:
 	if x in vowels:
 		numvowels += 1
-	elif x not in "1234567890 -=_+\][';/.,!@#$%^&*()`~<>?:{}|":	
+	elif x not in "=_+\][';/@#$%^&*()`~<>:{}|":	
 		numconsanants += 1
+		consonantarray.append(x)
 	elif x not in """1234568790 .,!?$()@#%^&*'""'""":
 		print("Please stop trying to break the program.")
-print("Vowels = %d" % numvowels)
-print("Consonants = %d" % numconsanants)
-time.sleep(5)
+for y in consonantarray:
+	consonants += y
+print("Vowels > %d" % numvowels)
+print("Consonants > %d" % numconsanants)
+print("User input without consonants > %s" % consonants)
+EOF = input("Press enter to continue...")
